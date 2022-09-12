d9b781ca432d87cbc878692ecbe37c13b592afdf
Ajaysdhillon <ajaysdhillon@gmail.com>
2022-09-12 14:56:17 +0530

    Adding additional routes and navbar component

--- src/App.js
+++ src/App.js
@@ -5,9 +5,15 @@ import { Box } from "@mui/material";
 const App = () => (
   <BrowserRouter>
     <Box sx={{ backgroundColor: "#000" }}>
-      Navbar
+      <Navbar />
       <Routes>
         <Route path="/" exact element={<Feed />}></Route>
+        <Route path="/video/:id" exact element={<VideoDetail />}></Route>
+        <Route
+          path="/search/:searchTerm"
+          exact
+          element={<SearchFeed />}
+        ></Route>
       </Routes>
     </Box>
   </BrowserRouter>
