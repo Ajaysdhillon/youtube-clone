34dca03398635ce411e90d6a4e08d00d5cbc2c99
Ajaysdhillon <ajaysdhillon@gmail.com>
2022-09-17 23:28:57 +0530

    Creating a channel detail component to display the details of the respective channels and also the videos

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -16,7 +16,7 @@ export const logo = "https://i.ibb.co/s9Qys2j/logo.png";
 
 export const categories = [
   { name: "New", icon: <HomeIcon /> },
-  /* { name: "JS Mastery", icon: <CodeIcon /> },*/
+  { name: "JS Mastery", icon: <CodeIcon /> },
   { name: "Html", icon: <CodeIcon /> },
   { name: "Css", icon: <CodeIcon /> },
   { name: "Data Structures", icon: <CodeIcon /> },
